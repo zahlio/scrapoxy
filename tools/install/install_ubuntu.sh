ba140085555b61f8783b7e92bb2761afab365c91
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-11 21:13:33 +0100

    fix(ova): change install_ubuntu inside link

--- tools/install/install_ubuntu.sh
+++ tools/install/install_ubuntu.sh
@@ -5,9 +5,9 @@ curl --silent --location https://deb.nodesource.com/setup_0.12 | bash -
 apt-get install --yes nodejs
 
 # Install proxy
-curl https://gist.githubusercontent.com/fabienvauchelles/41ed9142368bff9ba796/raw/505f6037cf2f6496552cd912b321fdf2faed9842/proxy.js > /root/proxy.js
+curl https://raw.githubusercontent.com/fabienvauchelles/scrapoxy/master/tools/install/proxy.js > /root/proxy.js
 
 # Install at startup
-curl https://gist.githubusercontent.com/fabienvauchelles/41ed9142368bff9ba796/raw/505f6037cf2f6496552cd912b321fdf2faed9842/proxyup.sh > /etc/init.d/proxyup.sh
+curl https://raw.githubusercontent.com/fabienvauchelles/scrapoxy/master/tools/install/proxyup.sh > /etc/init.d/proxyup.sh
 chmod a+x /etc/init.d/proxyup.sh
 update-rc.d proxyup.sh defaults
