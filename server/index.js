1f1db9a6419c29b4b734e77466361a47e02ae686
Fabien Vauchelles <fabien@vauchelles.com>
2018-08-03 17:30:59 +0200

    feat(release): change release version to 3.1.0

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ winston.add(winston.transports.Console, {timestamp: true});
 
 
 program
-    .version('3.0.1')
+    .version('3.1.0')
     .option('-d, --debug', 'Debug mode (increase verbosity)', debugMode)
     .parse(process.argv);
 
